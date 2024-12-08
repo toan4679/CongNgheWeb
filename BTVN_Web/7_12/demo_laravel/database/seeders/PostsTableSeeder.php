78f345b6c817606e9b37acd737ffb3683f90c2bd
toan4679 <toan4679@gmail.com>
2024-12-08 19:35:11 +0700

    btvn

---
+++ BTVN_Web/7_12/demo_laravel/database/seeders/PostsTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use Illuminate\Support\Facades\DB;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+        for ($i = 0; $i < 100; $i++) {
+        DB::table('posts')->insert([
+        'title' => $faker->sentence,
+        'content' => $faker->paragraph
+        ]);
+        }
+    }
+}
